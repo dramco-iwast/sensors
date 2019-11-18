c213160a6fe839f71a71905462a076257bb3c8f5
geoffreyottoy <geoffrey.ottoy@kuleuven.be>
2019-11-18 10:38:56 +0100

    Removed remote directories (see .gitignore)

--- firmware/ButtonsModule/IWAST_STEM.X/build/default/production/buttons.p1.d
+++
@@ -1,8 +0,0 @@
-build/default/production/buttons.p1:  \
-buttons.c  \
-buttons.h  \
-mcc_generated_files/mcc.h  \
-mcc_generated_files/device_config.h  \
-mcc_generated_files/pin_manager.h  \
-mcc_generated_files/interrupt_manager.h  \
-mcc_generated_files/i2c1.h 
